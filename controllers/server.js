57172444cdf45627cf3b0cd90abb0e9c5fe4e646
Samantha Karas <samanthakaras1@gmail.com>
2023-03-27 08:34:43 -0400

    structuring starter code

--- controllers/server.js
+++ controllers/server.js
@@ -2,15 +2,28 @@ const express = require('express');
 const app = express();
 
 // Index route
-app.get('/budgets', (req, res) => {
-  res.json(budgets);
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
 });
+
+app.get('/budgets', (req, res) => {
+  res.send('this is my budgets');
+});
+
+// Middleware?
+// app.set('view engine', 'ejs')
 
 // Show route
 
+app.get('/budgets/:index', (req,res) => {
+  res.send('budgets index');
+});
 
 // New route
 
+app.get('/budgets/:new', (req,res) => {
+  res.send('/budgets/new');
+});
 
 // Create route
 
